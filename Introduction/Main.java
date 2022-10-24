bc07cda7f5e8d9999dec5bec9b975770ce1d13a7
nealwaga <waganeal@gmail.com>
2022-10-25 00:46:00 +0300

    printing texts

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I am learning Java.");
-        System.out.println("It is awesome so far");
+        System.out.println("It is awesome so far.");
     }
 }
