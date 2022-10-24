5e33a93a5d6215b07bb1f680a74731962d02b3a7
nealwaga <waganeal@gmail.com>
2022-10-25 00:55:05 +0300

    comments

--- Introduction/Main.java
+++ Introduction/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        /*The code below will print the words Hello World
+        to the screen, and it is amazing*/
         System.out.println("Hello World!");
 
         System.out.print("I am learning Java. ");
@@ -8,3 +10,5 @@ public class Main {
         System.out.print(3 * 3);
     }
 }
+
+// This is a comment
