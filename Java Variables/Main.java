7f34b3bd63ead8e85211588d2426b5bfc8fa9c62
nealwaga <waganeal@gmail.com>
2022-10-25 08:08:09 +0300

    variables

--- Java Variables/Main.java
+++ Java Variables/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
       String name = "Neal";
       System.out.println(name);
+
+      // You can also declare a variable without assigning the value, and assign the value later:
+      int myNum;
+      myNum = 22;
+      System.out.println(myNum);
     }
   }
   
