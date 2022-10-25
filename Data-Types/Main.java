bd4558b5909528347e35b73d5642c0cb6c69c358
nealwaga <waganeal@gmail.com>
2022-10-25 12:33:36 +0300

    numbers

--- Data-Types/Main.java
+++ Data-Types/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main (String[] args) {
         int myNum = 1000;
         System.out.println(myNum);
+
+        float aNum = 5.75f;
+        System.out.println(aNum);
+
+        double theNum = 19.99d;
+        System.out.println(theNum);
     }
 }
